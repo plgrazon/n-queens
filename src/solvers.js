8e41a16e480402948c60b4bad243d05f2eb1e319
plgrazon <plgrazon@gmail.com>
2018-05-02 19:56:30 -0700

    Start working on solvers

--- src/solvers.js
+++ src/solvers.js
@@ -13,10 +13,29 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
+window.findSolution = function(row, n, board, hasConflict, cb) {
+  if (row === n) {
+    return cb();
+  }
+  
+  // Are we stopping too early? Even if there's nowhere to place a piece
+  // on a given row, sholdn't we move on to the next row anyway?
+  
+  for (var i = 0; i < n; i++) {
+    board.togglePiece(row, i);
+    if (!board[hasConflict]()) {
+      var result = findSolution(row + 1, n, board, hasConflict, cb);
+      if (result) {
+        return result;      
+      }
+    }
+    board.togglePiece(row, i);
+  }
+};
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n: n});
+  var solution = findSolution(0, n, board, 'hasAnyRooksConflicts', () => board.rows());
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,8 +43,21 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var board = new Board({n: n});
+  var solutionCount = 0;
+  
+  findSolution(0, n, board, 'hasAnyRooksConflicts', () => solutionCount++);
+  
+  // for (var i = 0; i < n; i++) {
+  //   for (var j = 0; j < n; j++) {
+  //     var solution = findNRooksSolution(i, j);
+  //     if (solution !== undefined) {
+  //       solutions.push(solution);
+  //     }      
+  //   }
+  // }
 
+  // solutionCount = solutions.length;
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
